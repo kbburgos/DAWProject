57b32e431faba830c70c957931835dbf36ecb889
Jonathan Quintana <jonathan.quintana.solorzano@gmail.com>
2019-05-31 23:07:24 -0500

    UPDATE; se actualiza template para quitar archivos innecesarios

--- Template/templatemo_511_journey/js/index-js.js
+++ Template/templatemo_511_journey/js/index-js.js
@@ -1,36 +1,3 @@
-    /* Google Maps
-    ------------------------------------------------*/
-    var map = '';
-    var center;
-
-    function initialize() {
-        var mapOptions = {
-            zoom: 16,
-            center: new google.maps.LatLng(37.769725, -122.462154),
-            scrollwheel: false
-        };
-
-        map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
-
-        google.maps.event.addDomListener(map, 'idle', function() {
-          calculateCenter();
-      });
-
-        google.maps.event.addDomListener(window, 'resize', function() {
-          map.setCenter(center);
-      });
-    }
-
-    function calculateCenter() {
-        center = map.getCenter();
-    }
-
-    function loadGoogleMap(){
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDVWt4rJfibfsEDvcuaChUaZRS5NXey1Cs&v=3.exp&sensor=false&' + 'callback=initialize';
-        document.body.appendChild(script);
-    }
 
     /* DOM is ready
     ------------------------------------------------*/
@@ -50,9 +17,7 @@
             $.scrollTo('#tm-section-search', 300, {easing:'linear'});
         });
 
-        // Date Picker in Search form
-        var pickerCheckIn = datepicker('#inputCheckIn');
-        var pickerCheckOut = datepicker('#inputCheckOut');
+
 
         // Update nav links on scroll
         $('#tm-top-bar').singlePageNav({
@@ -73,6 +38,6 @@
             slidesToScroll: 1
         });
 
-        loadGoogleMap();                                       // Google Map
+                                         // Google Map
         $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
     });
