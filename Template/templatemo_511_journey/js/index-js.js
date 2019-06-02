7e4b88308e2afd13095ee8e7781e9042c0c4f024
Carlos Sesme Vera <carlossesme31@gmail.com>
2019-06-01 21:21:47 -0500

    Pantalla de inicio o principal basa en la plantilla

--- Template/templatemo_511_journey/js/index-js.js
+++ Template/templatemo_511_journey/js/index-js.js
@@ -1,8 +1,48 @@
 
+var map = '';
+      var center;
+
+      function initialize() {
+          var mapOptions = {
+              zoom: 16,
+              center: new google.maps.LatLng(-2.147733, -79.964542),
+              scrollwheel: false
+          };
+
+          map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
+
+          google.maps.event.addDomListener(map, 'idle', function() {
+            calculateCenter();
+            console.log("calculo centro")
+        });
+
+          google.maps.event.addDomListener(window, 'resize', function() {
+            map.setCenter(center);
+           
+        });
+      }
+
+      function calculateCenter() {
+          center = map.getCenter();
+          var marker = new google.maps.Marker({
+            position: center,
+            map: map,
+            title: 'Escuela Superior Politecnica del Litoral - ESPOL'
+          });
+      }
+
+      function loadGoogleMap(){
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB_uM0BgDIiri-RlGCU49uXY7IRqMpcfcY&v=3.exp&' + 'callback=initialize';
+          document.body.appendChild(script);
+      }
+
+
     /* DOM is ready
     ------------------------------------------------*/
     $(function(){
-
+      // $(".slider").not('.slick-initialized').slick();
         // Change top navbar on scroll
         $(window).on("scroll", function() {
             if($(window).scrollTop() > 100) {
@@ -30,14 +70,25 @@
             $('#mainNav').removeClass('show');
         });
 
-        // Slick Carousel
-        $('.tm-slideshow').slick({
-            infinite: true,
-            arrows: true,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        });
-
+        //Slick Carousel
+        // $('.tm-slideshow').slick({
+        //     infinite: true,
+        //     arrows: true,
+        //     slidesToShow: 1,
+        //     slidesToScroll: 1
+        // });
+        loadGoogleMap();    
                                          // Google Map
+
+        //animacion scroll
+    $(".ancla").click(function(evento){
+      //Anulamos la funcionalidad por defecto del evento
+      evento.preventDefault();
+      //Creamos el string del enlace ancla
+      var codigo = "#" + $(this).data("ancla");
+      //Funcionalidad de scroll lento para el enlace ancla en 2 segundos
+      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+    });
+
         $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
     });
