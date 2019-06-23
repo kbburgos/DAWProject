f57b8855d396e903915a40c039b6d7c04792cac7
Tony Veas <tveas@espol.edu.ec>
2019-06-23 17:11:49 -0500

    arreglando vistas

--- publica/admin/sis-medic-tbs/src/app/app-routing.module.ts
+++ publica/admin/sis-medic-tbs/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //rutas personalizadas
+import { MenuComponent } from './menu/menu.component';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ViewExaComponent } from './view-exa/view-exa.component';
@@ -16,11 +18,12 @@ import { NewPacientComponent } from './newpacient/newpacient.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'view_exa', component: ViewExaComponent},
+  {path: 'view-exa', component: ViewExaComponent},
   {path: 'medic', component: MedicComponent},
   {path: 'newmedic', component: NewmedicComponent},
   {path: 'user', component: UserComponent},
   {path: 'reservations', component: ReservationsComponent},
+  {path: 'menu', component: MenuComponent},
   {path: 'home', component: HomeComponent},
   {path: 'newuser', component: NewUserComponent},
   {path: 'oldreservations', component: OldReservationsComponent},
