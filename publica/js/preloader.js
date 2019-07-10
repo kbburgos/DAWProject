43484f7698c68fcbf012b436ee2360286cb8ab1c
Carlos Sesme Vera <carlossesme31@gmail.com>
2019-07-10 01:46:28 -0500

    actualizacion contendio pagina estaica

--- publica/js/preloader.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-
-$(window).on('load',function() {
-    $('.preloader-wrapper').fadeOut();
-    $('body').removeClass('preloader-site');
-});
