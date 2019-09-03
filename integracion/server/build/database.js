1c6afb7fa13e6dd1e293fe228cbb53b8ed9624d4
Carlos Sesme Vera <carlossesme31@gmail.com>
2019-09-03 02:09:10 -0500

    UPDATE: consumo de servicios de examnes, base noSQL

--- integracion/server/build/database.js
+++ integracion/server/build/database.js
@@ -1,7 +1,7 @@
 "use strict";
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
+    useNewUrlParser: true, dbName: 'sismedic_daw'
 }).then((db) => { console.log("mongodb is connected"); }).
     catch((err) => {
     console.error(err);
