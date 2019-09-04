5a24f6e077af4752c3ea638049d75dfaec18f9c1
kbburgos <kbburgos@espol.edu.ec>
2019-09-04 11:22:34 -0500

    Arreglo de modulo perfil de usuario html

--- publica/admin/sis-medic-tbs/src/app/perfil/perfil.component.ts
+++ publica/admin/sis-medic-tbs/src/app/perfil/perfil.component.ts
@@ -10,25 +10,6 @@ export class PerfilComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $('#numero1').on('click', function() {
-      // $('#entrada').val($(this).attr('id'));
-      $('#parainfo').show();
-      $('#paraconfiguracion').hide();
-    });
-
-    $('#numero2').on('click', function() {
-      // $('#entrada').val($(this).attr('id'));
-      $('#parainfo').hide();
-      $('#paraconfiguracion').show();
-    });
-
-
-    $('#botoncito').on('click', function() {
-      alert("Los datos fueron guardados");
-    });
-
-
-
 
   }
 
