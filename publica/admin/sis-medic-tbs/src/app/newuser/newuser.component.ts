6de03a45b77118ad4007aaaf8c2999846b545ce2
kbburgos <kbburgos@espol.edu.ec>
2019-09-04 23:36:23 -0500

    Update configuracion perfil de usuario

--- publica/admin/sis-medic-tbs/src/app/newuser/newuser.component.ts
+++ publica/admin/sis-medic-tbs/src/app/newuser/newuser.component.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 import {AllServices} from './../services/AllServices';
 import { AuthService } from "../services/loginUtils/auth.service";
 import { DataService } from "./../services/data.services";
+import Encrypt from "../services/serealUtils/encrypt";
 //var CryptoJS = require("crypto-js");
 
 
@@ -13,11 +14,12 @@ import { DataService } from "./../services/data.services";
 })
 export class NewUserComponent implements OnInit {
 
-  constructor(private _services:AllServices,private _router: Router) { }
+  constructor(private _services:AllServices,private _router: Router,private login: AuthService, private data:DataService) { }
   usuario= { // deben crear una estructura de acuerdo al back para que se puedan registrar los datos, revisen los servicios que van a usar
     cedula:"",
     nombreUser:"",
     pasword: "",
+    password: "",
     apellidoUser:"",
     email:"",
     phone:"",
@@ -33,9 +35,11 @@ export class NewUserComponent implements OnInit {
   //createPacient(nombres:string,apellidos:string,ci:string,phone:string,email:string){ // crea el paciente revicen el html
   createUser(){
     this.isvisible = true;
+    let contra2 = ""+ $("#validationDefault04").val();
+    contra2= Encrypt.cifrar(contra2);
+    this.usuario.password= contra2;
     this._services.createUser(this.usuario).subscribe(data=>{
       this._router.navigate(["/user"])
-
     },err=>{
       console.log(err);
       this.isvisible = false
